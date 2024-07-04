b0fafdbdd0aa3a3d4d7470941480ad1e862554c6
ie-orphane <ilyasselyatime0@gmail.com>
2024-07-04 16:12:55 +0100

    add a lot

---
+++ template/expo/app/+notfound.jsx
@@ -0,0 +1,29 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <view className='flex-1 items-center justify-center'>
+        <Text type="title">This screen doesn't exist.</Text>
+        <Link href="/" style={styles.link}>
+          <Text type="link">Go to home screen!</Text>
+        </Link>
+      </view>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
---
+++ template/expo/app/_layout.jsx
@@ -0,0 +1,29 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
